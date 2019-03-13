a177c50012e4cb717d1068f133fe1c84e851c3b8
Dean McHugh <dean.mchugh1988@gmail.com>
2019-03-13 10:40:00 +1000

    routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom'
 import './App.css';
-import Navbar from './navbar/Navbar'
 import Bio from './bio/Bio'
+import Contacts from './contacts/Contacts'
+import Default from './Default'
+import Footer from './footer/Footer'
+import {Paintings, Drawings, Illustrations, Studies} from './gallary/Gallary'
+import Navbar from './navbar/Navbar'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <Bio />
+        <Switch>
+          <Route exact path='/' component={Bio}/>
+          <Route path='/contacts' component={Contacts}/>
+          <Route path='/paintings' component={Paintings}/>
+          <Route path='/drawings' component={Drawings}/>
+          <Route path='/illustrations' component={Illustrations}/>
+          <Route path='/studies' component={Studies}/>
+          <Route component={Default}/>
+        </Switch>
+        <Footer />
       </div>
     );
   }
