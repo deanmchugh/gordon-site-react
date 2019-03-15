c67d23aa5049cc833ab0090ae3e820c77f3db102
Dean McHugh <dean.mchugh1988@gmail.com>
2019-03-16 09:42:13 +1000

    background image

--- src/components/gallary/models/ImageCard.js
+++ src/components/gallary/models/ImageCard.js
@@ -3,17 +3,19 @@ import './imageCard.css'
 
 export default class ImageCard extends Component {
   render() {
+
+    var background = "url('{this.props.photo}')"
+
     return (
       <div className='card'>
         <div className='img-wrap'>
-          <figure className='gallary-item'>
-            <img className='photo' style={{display: !this.props.photo && 'none'}} src={this.props.photo} alt='artwork'/>
-          </figure>
+          <div style={{backgroundImage: background}}>
+          </div>          
           <div className='img-description'>
             <div className='img-text'>
-              <h1 className='title' style={{display: !this.props.title && 'none'}}>{this.props.title}</h1>
-              <h2 className='media' style={{display: !this.props.media && 'none'}}>{this.props.media}</h2>
-              <h2 className='size' style={{display: !this.props.size && 'none'}}>{this.props.size}</h2>
+              <h1 style={{display: !this.props.title && 'none'}}>{this.props.title}</h1>
+              <h2 style={{display: !this.props.media && 'none'}}>{this.props.media}</h2>
+              <h2 style={{display: !this.props.size && 'none'}}>{this.props.size}</h2>
             </div>
           </div>
         </div>
