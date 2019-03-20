e8e522cc7f7da15a599b51de8c3da6ba9547f692
Dean McHugh <dean.mchugh1988@gmail.com>
2019-03-20 15:59:50 +1000

    sixing

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import './contacts.css'
 
 export default class Contacts extends Component {
   render() {
     return (
-      <div>
-        <p>email</p>
-        <p>insta</p>
+      <div className='contact-display'>
+        <a href='https://www.instagram.com/lisagordonart/' target='_blank' rel='noopener noreferrer'>
+          <h1>INSTAGRAM: lisagordonart</h1></a>
+        <a href='mailto:lisagordonart@gmail.com' target='_blank' rel='noopener noreferrer'>
+          <h1>EMAIL: lisagordonart@gmail.com</h1></a>
       </div>
     )
   }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,30 +5,36 @@ import menuIcon from './img/menu.png'
 
 export default class Navbar extends Component {
   render() {
+
+    function toggleMenu() {
+      const navs = document.querySelectorAll('.navbar-item')
+      navs.forEach(nav => nav.classList.toggle('navbar-item-toggle'))
+    }
+
     return (
       <nav className='navbar'>
         <div className='navbar-home-item'>
           <Link to='/' alt='home'>
-            <button>LISA GORDON</button>
+            <button className='name'>LISA GORDON</button>
           </Link>
-          <div className='navbar-menu-item'>
+          <div className='navbar-toggle' onClick={toggleMenu}>
             <img src={menuIcon} alt='menu'></img>
           </div>
         </div>
         <div className='navbar-item'>
-          <Link to='/paintings' alt='paintings'>
+          <Link to='/paintings' alt='paintings' onClick={toggleMenu}>
             <button>PAINTINGS</button>
           </Link>
-          <Link to='/drawings' alt='drawings'>
+          <Link to='/drawings' alt='drawings' onClick={toggleMenu}>
             <button>DRAWINGS</button>
           </Link>
-          <Link to='/illustrations' alt='illustrations'>
+          <Link to='/illustrations' alt='illustrations' onClick={toggleMenu}>
             <button>ILLUSTRATIONS</button>
           </Link>
-          <Link to='/studies' alt='studies'>
+          <Link to='/studies' alt='studies' onClick={toggleMenu}>
             <button>STUDIES</button>
           </Link>
-          <Link to='/contacts' alt='contacts'>
+          <Link to='/contacts' alt='contacts' onClick={toggleMenu}>
             <button>CONTACTS</button>
           </Link>
         </div>
