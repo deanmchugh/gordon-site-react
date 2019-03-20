4ec4d8eb65c3ebdfadf34d80810530528cb310f8
Dean McHugh <dean.mchugh1988@gmail.com>
2019-03-20 10:51:00 +1000

    colapsing menu

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 import './navbar.css'
+import menuIcon from './img/menu.png'
 
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className='navbar-display'>
-        <Link to='/' alt='home'>
-            <button className='home-button'>LISA GORDON</button>
-        </Link>
-        <Link to='/paintings' alt='paintings'>
+      <nav className='navbar'>
+        <div className='navbar-home-item'>
+          <Link to='/' alt='home'>
+            <button>LISA GORDON</button>
+          </Link>
+          <div className='navbar-menu-item'>
+            <img src={menuIcon} alt='menu'></img>
+          </div>
+        </div>
+        <div className='navbar-item'>
+          <Link to='/paintings' alt='paintings'>
             <button>PAINTINGS</button>
-        </Link>
-        <Link to='/drawings' alt='drawings'>
+          </Link>
+          <Link to='/drawings' alt='drawings'>
             <button>DRAWINGS</button>
-        </Link>
-        <Link to='/illustrations' alt='illustrations'>
+          </Link>
+          <Link to='/illustrations' alt='illustrations'>
             <button>ILLUSTRATIONS</button>
-        </Link>
-        <Link to='/studies' alt='studies'>
+          </Link>
+          <Link to='/studies' alt='studies'>
             <button>STUDIES</button>
-        </Link>
-        <Link to='/contacts' alt='contacts'>
+          </Link>
+          <Link to='/contacts' alt='contacts'>
             <button>CONTACTS</button>
-        </Link>
+          </Link>
+        </div>
       </nav>
     )
   }
